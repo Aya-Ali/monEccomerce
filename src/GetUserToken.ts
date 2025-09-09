223ffdc18c7d0ab4fd17fb00250df66183bbab2d
Aya-Ali <ayaali13477@gmail.com>
2025-09-09 15:55:42 +0300

    change Cookies

--- src/GetUserToken.ts
+++ src/GetUserToken.ts
@@ -2,9 +2,9 @@
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 export async function getUserToken() {
-
+    const TokenName = (process.env.NODE_ENV === 'production' ? '__Secure-next-auth.session-token' : 'next-auth.session-token')
     const cookiesData = await cookies()
-    const decodedToken = cookiesData.get('next-auth.session-token')?.value
+    const decodedToken = cookiesData.get(TokenName)?.value
 
     const data = await decode({ token: decodedToken, secret: process.env.NEXTAUTH_SECRET! })
     return data?.accessToken
